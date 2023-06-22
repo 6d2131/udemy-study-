a57b0e24558529fdd0345f2b270febcb59e6661d
6d2131 <6d2131@gmail.com>
2023-06-23 02:55:37 +0900

    php-データ型について

--- 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
+++ 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
@@ -1,1 +1,16 @@
 <?php 
+// データ型とは
+$i = 1;
+// var_dump($i);
+$b = true;
+// var_dump($b);
+$str  = 'hello';
+// var_dump($str);
+
+// データ型の確認方法　var_dump
+
+// 異なる型は自動的に変換される
+echo $i + $b;
+// 型の取り扱いの注意
+var_dump($i === 1);
+var_dump($i === '1');
