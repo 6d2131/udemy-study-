2d950dc65fe673bbb42ab6effbf117b5461e053f
6d2131 <6d2131@gmail.com>
2023-06-24 00:09:56 +0900

    php-条件分岐

--- 030_PHP基礎Part1/0700_条件分岐を作成してみようPart1/start/index.php
+++ 030_PHP基礎Part1/0700_条件分岐を作成してみようPart1/start/index.php
@@ -1,1 +1,19 @@
 <?php 
+// if(true){
+
+// }   
+// elseif(true){
+
+// }
+// else {
+
+// };
+$i = 50;
+$kesseki = 5;
+if($i < 50 || $kesseki >= 5) {
+    echo '不合格';
+}elseif($i >= 50 && $i < 70){
+    echo '合格';
+}else{
+    echo '秀';
+}
--- 030_PHP基礎Part1/0800_条件分岐を作成してみようPart2/start/index.php
+++ 030_PHP基礎Part1/0800_条件分岐を作成してみようPart2/start/index.php
@@ -1,1 +1,19 @@
 <?php 
+// ==と===の違い
+if(1 === '1') {
+    echo 'true';
+} else {
+    echo 'false';
+}
+
+// ==は型の比較をしない
+if(1 == '1') {
+    echo 'true';
+} else {
+    echo 'false';
+}
+
+// falseな値
+// 0(数値、文字列)
+// NULL
+// FALSE
