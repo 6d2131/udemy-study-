0cff1f7e01a0e7876cb8d2d715aa00dab95319db
6d2131 <6d2131@gmail.com>
2023-06-30 03:46:11 +0900

    php-正規表現

---
+++ 040_PHP基礎Part2/0500_正規表現を使ってみよう/start/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$char = 'aAabd1_sscc';
+if(preg_match('/aAa/', $char, $result)) {
+    echo '検索成功';
+    print_r($result);
+} else {
+    echo '検索失敗';
+}
+
+$char = 'aAabd1_sscc';
+if(preg_match('/\wAa/', $char, $result)) {
+    echo '検索成功';
+    print_r($result);
+} else {
+    echo '検索失敗';
+}
--- 040_PHP基礎Part2/0600_理解度チェック（正規表現）/start/index.php
+++ 040_PHP基礎Part2/0600_理解度チェック（正規表現）/start/index.php
@@ -29,6 +29,13 @@
  * 124-56789 -> NG
  */
 
+// $num = '001-0012';
+// if(preg_match ('/^\d{3}-\d{4}$/', $num, $result)) {
+//     echo '検索成功';
+//     print_r($result);
+// } else {
+//     echo '一致なし';
+// }
 
 /**
  * Email
@@ -39,9 +46,37 @@
  * example-0.00@ex.co.jp -> OK
  * example/0.00@ex.co.jp -> NG
  */
-
+$email = 'example/0.00@ex.co.jp';
+if(preg_match('/^[\w.\-]+@[\w\-]+\.[\w.\-]+$/', $email, $result)){
+    echo 'OK';
+    print_r($result);
+} else {
+    echo '不正です。';
+}
 
 /**
  * HTML
  * 見出しタグ(h1~h6)の中身のみ取得してみよう。
  */
+$html = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>見出し1</h1>
+    <h2>見出し２</h2>
+    <h3>見出し3</h3>
+    <h4>見出し4</h4>
+    <h5>見出し５</h5>
+    <h6>見出し6</h6>
+</body>
+</html>';
+if(preg_match_all('/<h[1-6]>(.+)<\/h[1-6]>/',$html, $result)) {
+    echo '成功';
+    print_r($result);
+} else {
+    echo '無理';
+}
