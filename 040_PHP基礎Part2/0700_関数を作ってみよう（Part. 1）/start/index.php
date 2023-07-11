3f901854860813b25c78065439bf858b27064db2
6d2131 <6d2131@gmail.com>
2023-07-12 04:58:01 +0900

    php-関数

--- 040_PHP基礎Part2/0700_関数を作ってみよう（Part. 1）/start/index.php
+++ 040_PHP基礎Part2/0700_関数を作ってみよう（Part. 1）/start/index.php
@@ -1,1 +1,14 @@
 <?php
+$numbers = [1, 2, 3, 4];
+$numbers2 = [1, 2, 3];
+
+function sum($numbers) {
+    $sum = 0;
+    foreach($numbers as $num) {
+        $sum = $num;
+    }
+    return $sum;
+}
+
+$result = sum($numbers);
+echo "合計: {$result}<br>";
