ca52ff91de9a33466858a9cd18cc3fe77b5d76f4
6d2131 <6d2131@gmail.com>
2023-07-12 04:58:47 +0900

    php-scope

--- 040_PHP基礎Part2/1000_スコープについて理解しよう/start/index.php
+++ 040_PHP基礎Part2/1000_スコープについて理解しよう/start/index.php
@@ -1,1 +1,16 @@
 <?php
+//グローバルスコープ
+$a = 0;
+
+//ローカルスコープ
+function fn1() {
+    $b = 1;
+}
+
+function fn2() {
+    global $a;
+    var_dump($_SERVER); //スーパーグローバル
+    // $b = 2;
+    // echo $a;
+}
+fn2();
